1adaeb995917d419bceed3c37a79707339eb2718
Ellyll <cawsllyffant-git@yahoo.com>
2021-01-23 13:50:19 +0000

    Tidy calling of commands

--- FellyRobot.fsx
+++ FellyRobot.fsx
@@ -17,8 +17,14 @@ type Settings =
         Channels: string[]
     }
 
+// Get configuration
 let json = File.ReadAllText("Config.json")
 let settings = Json.deserialize<Settings> json
+let host = DnsEndPoint(settings.Host, settings.Port)
+let nick, user = settings.Nick, settings.User
+let channels = settings.Channels
+let password = settings.Password
+
 
 let isCommand (message: string) (command: string) =
     if String.IsNullOrWhiteSpace(message) then
@@ -27,27 +33,38 @@ let isCommand (message: string) (command: string) =
         let m = message.Trim().ToLower()
         m = command || m.StartsWith(command + " ")
 
-let host = DnsEndPoint(settings.Host, settings.Port)
-let nick, user = settings.Nick, settings.User
-let channels = settings.Channels
-let password = settings.Password
+// Command functions
+let hello (sender: string) (target: string) (message: string) =
+    printfn "Received !hello command"
+    Some <| IrcMessage.privmsg channels (sprintf "Hello %s!" sender)
+
+let commands =
+    [
+        ("!hello", hello)
+    ]
+    |> Map.ofList
 
 printfn "Opening connection...."
 let con = IrcConnection(host, nick, user, true)
 printfn "Connection opened!"
 do con.SendMessage (IrcMessage.pass password)
 
-// Transform an incoming message into a response with `Event.choose`, then send it
-// Greets the user upon receiving a direct message or a mention in a channel
+// Handle incomming messages
 con.MessageReceived
 |> Event.choose(function
     | PRIVMSG(Nickname sender, target, message) -> // when target = nick -> 
         printfn "Message 1 received: sender=\"%s\" target=\"%s\" \"%s\"" sender target message
-        if isCommand message "!hello" then
-            printfn "Received !hello command"
-            Some <| IrcMessage.privmsg channels (sprintf "Hello %s!" sender)
-        else
-            None
+        // Check if messages is a command, if it is execute it
+        let commandFunc =
+            commands |> Map.tryPick (fun commandName commandFunc ->
+            if isCommand message commandName then
+                Some commandFunc
+            else
+                None )
+        match commandFunc with
+        | Some fn -> fn sender target message
+        | None -> None
+
     | PING(_, server1, _) ->
         printfn "PING received from: server1=%s, sending PONG" server1
         Some <| IrcMessage.pong server1
@@ -57,11 +74,9 @@ con.MessageReceived
     //| _ -> None)
 |> Event.add(con.SendMessage)
 
-//do con.SendMessage (IrcMessage.pass settings.Password)
 do con.SendMessage (IrcMessage.join channels)
 do con.SendMessage (IrcMessage.privmsg channels "Hello, world!")
 
-//Application.Run()
 printfn "Running! Press any key"
 System.Console.ReadKey() |> ignore
 do con.SendMessage (IrcMessage.part channels)
